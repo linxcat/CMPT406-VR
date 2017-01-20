e45ce2882fb00d2c26e5958e364fc2c1b1301001
linxcat <clavoy_comp@hotmail.com>
2017-01-19 22:54:07 -0600

    Initial Work on Sigil positioning and locking. Rotation not working.

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+    public bool IS_PRIMARY; // script is present on both hands, but we need left-hand support
+
+    public float SIGIL_DISTANCE = 1.5F;
+
+    bool swordIsOn = true;
+    bool locked = false;
+
+    GameObject sigilAnchor;
+    GameObject swordAnchor;
+    GameObject centerEyeAnchor;
+    GameObject handAnchor;
+
+    // Use this for initialization
+	void Start () {
+        sigilAnchor = GameObject.Find("SigilAnchor");
+        swordAnchor = GameObject.Find("SwordAnchor");
+        centerEyeAnchor = GameObject.Find("CenterEyeAnchor");
+        handAnchor = GameObject.Find("Hand-Dominant");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateSigilAnchor();
+	}
+
+    void updateSigilAnchor()
+    {
+        if (!IS_PRIMARY || locked) return;
+
+        Vector3 sigilDirection = transform.position - centerEyeAnchor.transform.position;
+        sigilDirection.Normalize();
+        Vector3 sigilTransform = sigilDirection * SIGIL_DISTANCE;
+        sigilAnchor.transform.position = handAnchor.transform.position + sigilTransform;
+
+        sigilAnchor.transform.right = sigilDirection * -1; //rotation is broken.
+        sigilAnchor.transform.rotation = new Quaternion();
+    }
+
+    // switches the hand, if it's the dominant one, between sword and magic
+    public void switchMode()
+    {
+        if (!IS_PRIMARY) return; // only the primary hand can switch
+        swordIsOn = !swordIsOn;
+        swordAnchor.SetActive(swordIsOn);
+        sigilAnchor.SetActive(!swordIsOn);
+    }
+
+    public void lockSigils(bool locking)
+    {
+        if (!IS_PRIMARY) return;
+
+        locked = locking;
+
+    }
+
+    public void switchPrimaryHand()
+    {
+        IS_PRIMARY = !IS_PRIMARY;
+    }
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    Hand[] hands = new Hand[2];
+
+	// Use this for initialization
+	void Start () {
+        hands[0] = GameObject.Find("Hand-Dominant").GetComponent<Hand>();
+        hands[1] = GameObject.Find("Hand-Secondary").GetComponent<Hand>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool rightModeSwitch = OVRInput.GetDown(OVRInput.Button.One);
+        bool locking = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
+        if (rightModeSwitch) switchHandMode();
+        lockHand(locking);
+	}
+
+    void switchHandMode()
+    {
+        foreach (Hand hand in hands)
+        {
+            hand.switchMode();
+        }
+    }
+
+    void lockHand(bool locking)
+    {
+        foreach (Hand hand in hands)
+        {
+            hand.lockSigils(locking);
+        }
+    }
+}
