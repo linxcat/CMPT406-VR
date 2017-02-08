7044cd8c0f49be9dcc3f1bd3e7ee0bb596c491fb
Jordan Wong <cooldued59@gmail.com>
2017-02-07 22:27:41 -0600

    Inital Commit for GUI in Prod

---
+++ Assets/Scripts/GUIPublisher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace GUIPubSub {
+
+    public class GUIPublisher{
+        private List<GUISubscriber> subscribers;
+
+        private GUIPublisher() {
+            subscribers = new List<GUISubscriber>();
+        }
+
+        public static GUIPublisher create() {
+            return new GUIPublisher();
+        }
+
+        public void Subscribe(GUISubscriber sub) {
+            subscribers.Insert(0, sub);
+        }
+
+        private void publish(GUIEvent data) {
+            foreach (GUISubscriber sub in subscribers) {
+                sub.getPublication(data);
+            }
+        }
+
+
+        //TODO: Get event 
+    }
+}
---
+++ Assets/Scripts/GUISubscriber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUIPubSub {
+
+    public interface GUISubscriber {
+        void getPublication(GUIEvent e);
+    }
+    public class GUIHealthSubscriber : GUISubscriber {
+
+        public void getPublication(GUIEvent e) {
+            throw new NotImplementedException();
+        }
+    }
+
+}
