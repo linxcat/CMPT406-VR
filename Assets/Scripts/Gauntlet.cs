b594690df018365156aa470b4e661068a38a713b
ANGUS JO KWANG <ANGUS JO KWANG>
2017-02-09 13:14:00 -0600

    Added functions for OnTriggerEnter in gauntletScript
    Checks collider's tag, check speed of gauntlet, then call counter
    function
    Since enemy and projectile script doesn't exists, the calls are
    commented out
    Once they are created, rename the object names accordingly
    Needs further testing on speedCheck

---
+++ Assets/Scripts/Gauntlet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gauntlet : MonoBehaviour {
+
+    float validSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check tag
+        if(other.tag == "Enemy")
+        {
+            //Call enemy's counter function
+            //EnemyScript enemyscript = other.gameObject.GetComponent<EnemyScript>();
+            if (speedCheck(validSpeed))
+            {
+                //enemyscript.counter();
+            }
+        }else if (other.tag == "projectile")
+        {
+            //call projectile's counter function
+            //ProjectileScript projectilescript = other.gameObject.GetComponent<ProjectileScript>();
+            if (speedCheck(validSpeed))
+            {
+                //projectilescript.counter();
+            }
+        }
+    }
+
+    private bool speedCheck(float s)
+    {
+        float speed = this.GetComponent<Rigidbody>().velocity.magnitude;
+        return speed > s;
+    }
+}
