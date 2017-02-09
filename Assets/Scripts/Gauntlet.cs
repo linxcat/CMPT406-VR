12ce791c9d44a545dbab29e5ae17a38a5df29f98
Angus <angus0530@hotmail.com>
2017-02-09 17:02:31 -0600

    Gauntlet checks average speed in 10 frames
    Now calculates average speed of past 10 frames and speed check with it
    prompts debug messages for debug purposes
    differentiate weapon from enemy so it recognizes punch

--- Assets/Scripts/Gauntlet.cs
+++ Assets/Scripts/Gauntlet.cs
@@ -4,43 +4,60 @@ using UnityEngine;
 
 public class Gauntlet : MonoBehaviour {
 
-    float validSpeed = 10;
+    private float validSpeed = 10;
+	private float[] pastSpeed;
+	private int counter;
+	private float totalSpeed, avgSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
+		pastSpeed = new float[10];
+		counter = 0;
+		for(int i = 0; i < 10; i++){
+			pastSpeed [i] = 0;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		pastSpeed [counter++] = this.GetComponent<Rigidbody> ().velocity.magnitude;
+		if (counter == 10)
+			counter = 0;
+		totalSpeed = 0;
+		for(int i = 0; i < 10; i++){
+			totalSpeed += pastSpeed [i];
+		}
+		if (totalSpeed == 0)
+			avgSpeed = 0;
+		avgSpeed = totalSpeed / 10;
 	}
 
     private void OnTriggerEnter(Collider other)
-    {
-        //check tag
-        if(other.tag == "Enemy")
-        {
-            //Call enemy's counter function
-            //EnemyScript enemyscript = other.gameObject.GetComponent<EnemyScript>();
-            if (speedCheck(validSpeed))
-            {
-                //enemyscript.counter();
-            }
-        }else if (other.tag == "projectile")
-        {
-            //call projectile's counter function
-            //ProjectileScript projectilescript = other.gameObject.GetComponent<ProjectileScript>();
-            if (speedCheck(validSpeed))
-            {
-                //projectilescript.counter();
-            }
-        }
+	{				
+		Debug.Log("Gauntlet speed check with: " + avgSpeed);
+		if (speedCheck (validSpeed)) {
+			//check tag
+			if (other.tag == "Weapon") {
+				//Call enemy's counter function
+				//EnemyScript enemyscript = other.gameObject.GetComponent<EnemyScript>();
+				//enemyscript.counter();
+				Debug.Log("Gauntlet counter weapon");
+			} else if (other.tag == "projectile") {
+				//call projectile's counter function
+				//ProjectileScript projectilescript = other.gameObject.GetComponent<ProjectileScript>();
+				//projectilescript.counter();
+				Debug.Log("Gauntlet counter projectile");
+			} else if (other.tag == "Enemy") {
+				//Punch enemy
+				//EnemyScript enemyscript = other.gameObject.GetComponent<EnemyScript>();
+				//enemyscript.punch();
+				Debug.Log("Gauntlet punch enemy");
+			}
+		}
     }
 
     private bool speedCheck(float s)
     {
-        float speed = this.GetComponent<Rigidbody>().velocity.magnitude;
-        return speed > s;
+		return avgSpeed > s;
     }
 }
