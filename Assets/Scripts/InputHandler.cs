bffb6ed070e8090738dee68076763c3e45872e06
linxcat <clavoy_comp@hotmail.com>
2017-02-14 20:20:16 -0600

    Commiting as Cone is created, going to make it follow the player

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class InputHandler : MonoBehaviour {
 
     Hand[] hands = new Hand[2];
+    Teleport teleport;
+    float thumbstickDeadzone = 0.1F;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         hands[0] = GameObject.Find("Hand-Dominant").GetComponent<Hand>();
         hands[1] = GameObject.Find("Hand-Secondary").GetComponent<Hand>();
-	}
+        teleport = GameObject.Find("Player").GetComponent<Teleport>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         bool modeSwitch = OVRInput.GetDown(OVRInput.Button.One);
         bool debugSwitch = OVRInput.GetDown(OVRInput.Button.Three);
         bool locking = OVRInput.Get(OVRInput.Button.SecondaryHandTrigger);
+        Vector2 thumbstickAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick); // TODO hand switching
+        bool thumbstickClick = OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick);
+
+        if (thumbstickPassedDeadzone(thumbstickAxis)) {
+            teleport.setRotation(thumbstickAxis);
+            if (thumbstickClick) teleport.go();
+        }
+        else
+            teleport.disable();
+
         if (modeSwitch) switchHandMode();
         if (debugSwitch) switchDebug();
         lockHand(locking);
-	}
+
+    }
 
     void switchHandMode()
     {
@@ -42,5 +56,9 @@ public class InputHandler : MonoBehaviour {
         {
             hand.setLock(locking);
         }
+    }
+
+    bool thumbstickPassedDeadzone(Vector2 thumbstickAxis) {
+        return (Mathf.Abs(thumbstickAxis.x) > thumbstickDeadzone || Mathf.Abs(thumbstickAxis.y) > thumbstickDeadzone);
     }
 }
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public const float FADE_DURATION = 0.2f; // TODO change all constants to constant type
+    private bool active = false;
+
+    GameObject player;
+    GameObject avatar;
+    Transform apex;
+    Transform groundLocation;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+        apex = GameObject.Find("apex").transform;
+        groundLocation = GameObject.Find("groundMarker").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active) {
+            // raycasting
+        }
+	}
+
+    public void go() {
+        StartCoroutine("TeleportPosition");
+    }
+
+    IEnumerator TeleportPosition() {
+        fadeOut();
+        avatar.SetActive(false); // otherwise we see hands in the black while teleporting
+        yield return new WaitForSeconds(FADE_DURATION);
+        avatar.SetActive(true);
+        player.transform.position = groundLocation.position;
+        player.transform.forward = groundLocation.forward;
+        fadeIn();
+    }
+
+    public void setRotation(Vector2 vector) {
+        active = true;
+    }
+
+    public void disable() {
+        active = false;
+    }
+
+    private void fadeOut() {
+
+    }
+
+    private void fadeIn() {
+
+    }
+}
