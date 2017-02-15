5f47413595d95e47ad0dfb7e026eff1bf346c4ca
linxcat <clavoy_comp@hotmail.com>
2017-02-14 20:24:31 -0600

    Someone made a class and then just renamed the file... I feel triggered

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class NewBehaviourScript : MonoBehaviour {
+public class CharacterStats : MonoBehaviour {
 
 
     public int PLAYER_HEALTH = 100;
