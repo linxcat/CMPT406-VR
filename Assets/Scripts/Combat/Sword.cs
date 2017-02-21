ad33f3d78be8b6410c5eb1c72e0aea47b82f15cb
Flowers <dbf258@usask.ca>
2017-02-20 18:13:05 -0600

    Accidentally made a second SwordImplementation branch. This will be the correct one. Added what I had for haptic feedback in the first branch to this one.

--- Assets/Scripts/Combat/Sword.cs
+++ Assets/Scripts/Combat/Sword.cs
@@ -26,13 +26,17 @@ public class Sword : MonoBehaviour {
 
     LinkedList<GameObject> enemyContacts = new LinkedList<GameObject>();
 
-    public bool swordCharged = false;
+    bool swordCharged = false;
+
+    public AudioClip vibeAudioClip;
+    OVRHapticsClip vibeClip;
 
     // Use this for initialization
     void Start () {
         swordAnchor = transform.parent.gameObject;
         hitArray = GameObject.Find("Hit Array").GetComponent<HitArray>();
-	}
+        vibeClip = new OVRHapticsClip(vibeAudioClip);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -41,10 +45,11 @@ public class Sword : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
         // if (other.gameObject.layer != LayerMask.NameToLayer("EnemyHit")) return; // sword layer only collides with enemy layer
-        if (isSwinging) enemyContacts.AddFirst(other.gameObject);
+        if (isSwinging) enemyContacts.AddFirst(other.gameObject); InitiateHapticFeedback(vibeClip, 1);
     }
 
     public void startSlash() {
+        InitiateHapticFeedback(vibeClip, 1);
         timeStep = 0;
         for (int i = 0; i < directionDeviations.Length; i++) {
             directionDeviations[i] = 0F;
@@ -71,6 +76,7 @@ public class Sword : MonoBehaviour {
     }
 
     public void stopSlash() {
+        InitiateHapticFeedback(vibeClip, 1);
         isSwinging = false;
         swordCharged = false;
         StopCoroutine("swordCharge");
@@ -199,5 +205,17 @@ public class Sword : MonoBehaviour {
             GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
         }
         debugMode = !debugMode;
+    }
+
+    //Call to initiate haptic feedback on a controller depending on the channel perameter. (Left controller is 0, right is 1)
+    public void InitiateHapticFeedback(OVRHapticsClip hapticsClip, int channel)
+    {
+        OVRHaptics.Channels[channel].Mix(hapticsClip);
+    }
+
+    public void InitiateHapticFeedback(AudioClip hapticsAudioClip, int channel)
+    {
+        OVRHapticsClip hapticsClip = new OVRHapticsClip(hapticsAudioClip);
+        OVRHaptics.Channels[channel].Mix(hapticsClip);
     }
 }
