0e8134fc921fa2a26465494c548cfe9abb419a7c
Jo Kwang <anj218@usask.ca>
2017-03-07 19:30:33 -0600

    First iteration of spawn system
    Bug:
    Enemy spawns at 0 0 0 instead of the intended spawn point

---
+++ Assets/Scripts/Level/SpawnPoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public enum enemyType
+    {
+        nothing,
+        runner,
+        ranged
+    }
+
+    public enemyType[] toSpawn;
+    private int waveCount;
+
+	// Use this for initialization
+	void Start () {
+        waveCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawn() {
+        Debug.Log(gameObject.name + "spawn " + toSpawn[waveCount] + " on " + transform.position.x+" "+ transform.position.y+" "+ transform.position.z);
+        switch (toSpawn[waveCount])
+        {
+            case enemyType.nothing:
+                break;
+            case enemyType.runner:
+                Instantiate(Resources.Load("runner"), transform.position, transform.rotation);
+                break;
+            case enemyType.ranged:
+                Instantiate(Resources.Load("ranged"), transform.position, transform.rotation);
+                break;
+            default:
+                break;
+        }
+
+        waveCount++;
+    }
+}
