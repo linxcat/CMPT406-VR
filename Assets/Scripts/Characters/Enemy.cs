2385741fef7852a5603f288c41903575e68d3731
Angus <angus0530@hotmail.com>
2017-03-13 14:36:45 -0600

    Moved files from enemyNavMeshTest branch to here
    Move to new branch based on prod to advoid crazy merge confilcts
    Added Level1, Level2
    Scaled down enemy size
    runner moves with nav mesh

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class Enemy : MonoBehaviour {
 
     //protected Material colourMaterial;
     protected GameObject player;
+    protected NavMeshAgent agent;
     public int hp = 100;
     public int maxDamage = 50;
 
@@ -18,6 +20,7 @@ public abstract class Enemy : MonoBehaviour {
 
     // Use this for initialization
     public void Start () {
+        agent = GetComponent<NavMeshAgent> ();
         audioSource = GetComponent<AudioSource>();
         player = GameObject.Find("Hitbox");
 	}
