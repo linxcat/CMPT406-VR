01554fba4eb99822cce944b7f30f07fcf61528cb
jox561 <jox561@mail.usask.ca>
2017-03-15 22:43:27 -0600

    Dylan made more sprites/ can go to next level now

--- Assets/RightHandTutorialBoard.cs
+++ Assets/RightHandTutorialBoard.cs
@@ -7,16 +7,20 @@ public class RightHandTutorialBoard : MonoBehaviour {
 
     Renderer ren1;
     Renderer ren2;
+    Renderer ren3;
 
     // Use this for initialization
     void Start () {
         GameObject board1 = GameObject.Find("RightBoard1");
         GameObject board2 = GameObject.Find("RightBoard2");
+        GameObject board3 = GameObject.Find("5");
 
         ren1 = board1.GetComponent<Renderer>();
         ren1.enabled = true;
         ren2 = board2.GetComponent<Renderer>();
         ren2.enabled = false;
+        ren3 = board3.GetComponent<Renderer>();
+        ren3.enabled = true;
     }
 	
 	// Update is called once per frame
@@ -25,10 +29,12 @@ public class RightHandTutorialBoard : MonoBehaviour {
             if (ren1.enabled) {
                 ren1.enabled = false;
                 ren2.enabled = true;
+                ren3.enabled = false;
             }
             else {
                 ren1.enabled = true;
                 ren2.enabled = false;
+                ren3.enabled = true;
             }
         }
 	}
---
+++ Assets/Tutorial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour {
+
+    float leaveCount;
+
+	// Use this for initialization
+	void Start () {
+        leaveCount = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger)) {
+            leaveCount += Time.deltaTime;
+            if(leaveCount > 2.0f) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        else {
+            leaveCount = 0f;
+        }
+	}
+}
