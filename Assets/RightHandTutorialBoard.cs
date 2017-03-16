22bf8af045f9d20dd3efb96d3b9135f846d38f5b
jox561 <jox561@mail.usask.ca>
2017-03-15 20:45:30 -0600

    Dylan made improved sprites / some script

---
+++ Assets/RightHandTutorialBoard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHandTutorialBoard : MonoBehaviour {
+    
+
+    Renderer ren1;
+    Renderer ren2;
+
+    // Use this for initialization
+    void Start () {
+        GameObject board1 = GameObject.Find("RightBoard1");
+        GameObject board2 = GameObject.Find("RightBoard2");
+
+        ren1 = board1.GetComponent<Renderer>();
+        ren1.enabled = true;
+        ren2 = board2.GetComponent<Renderer>();
+        ren2.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetDown(OVRInput.Button.One)) {
+            if (ren1.enabled) {
+                ren1.enabled = false;
+                ren2.enabled = true;
+            }
+            else {
+                ren1.enabled = true;
+                ren2.enabled = false;
+            }
+        }
+	}
+}
