4e1b8910766c55535f0023a3a4ad1e3103dfa475
SwiftSwrd <bjk386@mail.usask.ca>
2017-03-29 23:11:11 -0600

    Untested, fireball should now have a trail and explosion particles.

--- Assets/Scripts/Characters/Fireball.cs
+++ Assets/Scripts/Characters/Fireball.cs
@@ -8,6 +8,8 @@ public class Fireball : MonoBehaviour {
     float FLIGHT_SPEED = 18F;
     private float duration = 4f;
 
+    public GameObject explosion;
+
     // Use this for initialization
     void Start() {
         GetComponent<Rigidbody>().velocity = transform.forward.normalized * FLIGHT_SPEED;
@@ -15,16 +17,19 @@ public class Fireball : MonoBehaviour {
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy") || other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
-            foreach (Collider enemy in Physics.OverlapSphere(transform.position, 6f, LayerMask.GetMask("Enemy")))
-                burn(enemy);
-            Destroy(gameObject);
-        }
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy") || other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+          explode();
+    }
+
+    void explode() {
+        foreach (Collider enemy in Physics.OverlapSphere(transform.position, 6f, LayerMask.GetMask("Enemy")))
+            burn(enemy);
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 
     void burn(Collider other) {
-        if (other.tag != "TutorialEnemy") { //don't damage tutorial enemies
+        if (other.tag != "TutorialEnemy") //don't damage tutorial enemies
             other.SendMessage("startBurning", duration);
-        }
     }
 }
